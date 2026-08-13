f0f24d442911abc04edb4f7e8b93985cee1b033c
Lexot0 <405578615@qq.com>
2026-08-13 19:02:10 +0800

    Initial commit

---
+++ ext/app/conf.php
@@ -0,0 +1,13 @@
+<?php
+return array (
+	'id' => 'app',
+	'type' => 'ext',
+	'author' => '雪洛',
+	'name' => '应用中心',
+	'intro' => '官方应用中心，提供主题模板、扩展的更新与安装。',
+	'price' => 0,
+	'home' => 'https://prain.cn',
+	'version' => '1.1.0',
+	'limit' => '1.0.0',
+);
+?>
